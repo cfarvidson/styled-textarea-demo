5ba419e0491e003dbea2306d00ad52cd0b480173
Carl-Fredrik Arvidson <carl-fredrik@arvidson.io>
2023-10-31 12:32:07 +0100

    Adds a hackey way of solving the re-render issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { useState, useRef, useMemo } from "react";
 import ContentEditable from "react-contenteditable";
-import { useDebounce } from "react-use";
 
 import "./App.css";
 import { convertTextToHtml, convertToText, validateHtml } from "./utils";
@@ -8,51 +7,46 @@ import { convertTextToHtml, convertToText, validateHtml } from "./utils";
 const DEFAULT_VALUE = "<p>foo good<p><p>bar bad</p>";
 
 function App() {
-  const [htmlContent, setHtmlContent] = useState(validateHtml(DEFAULT_VALUE));
-  const ref = useRef(null);
+  const [htmlContent, setHtmlContent] = useState(DEFAULT_VALUE);
+  const ref = useRef<HTMLDivElement | null>(null);
 
   const text = useMemo(() => {
     return convertToText(htmlContent);
   }, [htmlContent]);
 
-  useDebounce(
-    () => {
-      const validated = validateHtml(htmlContent);
-      if (validated !== htmlContent) {
-        setHtmlContent(validated);
-      }
-    },
-    1000,
-    [htmlContent]
-  );
-
   return (
     <div className="h-screen w-screen flex justify-center p-10 flex gap-2">
       <div>
         <h2>Editable:</h2>
-        <ContentEditable
-          className="bg-white p-2 w-64 h-full text-left rounded border border-2"
-          innerRef={ref}
-          html={htmlContent}
-          onPaste={(e) => {
-            e.preventDefault();
-            const pastedText = e.clipboardData.getData("text/plain");
-            const html = convertTextToHtml(pastedText);
-            if (html) {
-              setHtmlContent(html);
-            }
-          }}
-          disabled={false}
-          onChange={(e) => {
-            setHtmlContent(e.target.value);
-          }}
-          tagName="div"
-        />
+        <div className="relative bg-transparent w-64  text-left rounded border border-2">
+          <ContentEditable
+            className="absolute w-full h-full p-2"
+            innerRef={ref}
+            html={htmlContent}
+            onPaste={(e) => {
+              e.preventDefault();
+              const pastedText = e.clipboardData.getData("text/plain");
+              const html = convertTextToHtml(pastedText);
+              if (html) {
+                setHtmlContent(html);
+              }
+            }}
+            disabled={false}
+            onChange={(e) => {
+              setHtmlContent(e.target.value);
+            }}
+            tagName="div"
+          />
+          <div
+            className="bg-white w-full h-full p-2 text-transparent"
+            dangerouslySetInnerHTML={{ __html: validateHtml(htmlContent) }}
+          />
+        </div>
       </div>
 
       <div className="flex flex-col">
         <h2>Text content:</h2>
-        <pre className="text-xs bg-gray-50 p-2 rounded w-64 h-full">{text}</pre>
+        <pre className="text-xs bg-gray-50 p-2 rounded w-64">{text}</pre>
       </div>
     </div>
   );
